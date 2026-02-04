d5128bb46069a86ba0cd51bac3b95cf9d4a8b6a4
roldanmatias441 <roldanmatias441@gmail.com>
2026-02-04 16:22:36 +0000

    ✨feat: implementar sistema de notificaciones push con Expofeat: implementar sistema de notificaciones push con Expo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,20 @@
-import { Stack } from 'expo-router';
-import { AuthProvider } from '../lib/modules/auth/AuthProvider';
+
+import { usePushNotifications } from "@/lib/core/notifications/usePushNotifications";
+import { AuthProvider, useAuth } from "@/lib/modules/auth/AuthProvider";
+import { Stack } from "expo-router";
+
+function AuthLayout() {
+  const { session } = useAuth();
+  const userId = session?.user.id;
+  usePushNotifications(userId);
+  return <Stack />;
+}
+
 
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <Stack screenOptions={{ headerShown: false }} />
+      <AuthLayout />
     </AuthProvider>
   );
 }
